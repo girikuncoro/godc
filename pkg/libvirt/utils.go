12ccc5c8fe9edd392e52049b4f17c623eae2479d
Giri Kuncoro <girikuncoro@gmail.com>
2018-10-24 22:58:51 +0700

    Add helper to generate disk name

--- pkg/libvirt/utils.go
+++ pkg/libvirt/utils.go
@@ -15,3 +15,16 @@ func xmlMarshallIndented(b interface{}) (string, error) {
 	}
 	return buf.String(), nil
 }
+
+var diskLetters = []rune("abcdefghijklmnopqrstuvwxyz")
+
+func diskLetterForIndex(idx int) string {
+	m := idx / len(diskLetters)
+	n := idx % len(diskLetters)
+	letter := diskLetters[n]
+
+	if m == 0 {
+		return fmt.Sprintf("%c", letter)
+	}
+	return fmt.Sprintf("%s%c", diskLetterForIndex(m-1), letter)
+}
