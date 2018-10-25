3004d15ba1056be0ef0af00c157de05e261521ad
Giri Kuncoro <girikuncoro@gmail.com>
2018-10-25 15:04:17 +0700

    Fix invalid owner and group permissions

--- pkg/libvirt/volume_def.go
+++ pkg/libvirt/volume_def.go
@@ -15,9 +15,7 @@ func newDefVolume() libvirtxml.StorageVolume {
 				Type: "qcow2",
 			},
 			Permissions: &libvirtxml.StorageVolumeTargetPermissions{
-				Mode:  "644",
-				Owner: "libvirt-qemu",
-				Group: "kvm",
+				Mode: "644",
 			},
 		},
 		Capacity: &libvirtxml.StorageVolumeSize{
